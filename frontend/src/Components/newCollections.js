cc665cd77a729c7e7111c960efa8dae2f8bc1407
Leslie Orellana <lesore0789@gmail.com>
2024-04-18 18:47:53 -0700

    initial cleanup and adding assets

---
+++ frontend/src/Components/newCollections.js
@@ -0,0 +1,69 @@
+import w31 from "./images/w31.jpg";
+import y81 from "./images/y81.jpg";
+import w51 from "./images/w51.jpg";
+import v81 from "./images/v81.jpg";
+import w61 from "./images/w61.jpg";
+import v21 from "./images/v21.jpg";
+import w81 from "./images/w81.jpg";
+import y11 from "./images/y11.jpg";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Vintage 90s Eddie Bauer Teal Navy Blue Purple Sweater Bomber Jacket Style Full Zip String Knot Athletic Track Outdoor Jacket Size XL",
+    image: w31,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "Vintage 90s Green Jewels & Tassel Versace Style Print Pattern Colorful Windbreaker Fancy Multicolor Full Zip Jacket Size Petite Medium",
+    image: y81,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Vintage 90s Purple With Teal And Black Striped Lining Detail Windbreaker Embroidered Tennis Ball & Racket Full Zip Sports Jacket Size 2X",
+    image: w51,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "Vintage 90s Shiny Gold Pullman Players Ramada Express Graphic Print Bomber Jacket Laughlin Nevada Snap Button Casino Jacket Size Large",
+    image: v81,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 15,
+    name: "Vintage 90s Sport Abouts Jacket Burgundy Red Maroon Zip Up Moto Biker Shoulder Neck Strap Snap Button Bomber Lightweight Jacket Size Large",
+    image: w61,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Vintage 80s Brown Abstract Geometric Print Pattern Short Sleeve Polo Shirt Grandpa Dad Style Dorky Garterized Hem Button Polo Size Medium",
+    image: v21,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Vintage 90s Black San Diego California Graphic Print Hoody Sweater Patchwork Style Color Block Palm Trees Pullover Hoodie Sweatshirt Size M",
+    image: w81,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 28,
+    name: "Vintage 90s 100% Silk Black Abstract Shields Versace Style Tapestry Print Pattern Bomber Jacket Full Zip Funky Windbreaker Size XL",
+    image: y11,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default new_collections;
