2cafac56f9d64a5f6bcd85039f8a59f5895c3822
Leslie Orellana <lesore0789@gmail.com>
2024-04-20 22:34:47 -0700

    structured and stylized the popular and item component

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import v71 from "./images/v71.jpg";
+import w71 from "./images/w71.jpg";
+import y21 from "./images/y21.jpg";
+import y11 from "./images/y11.jpg";
+
+let data_product = [
+  {
+    id: 1,
+    name: "PUMA Lightweight Windbreaker Colorful Striped Detail",
+    image: v71,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Bob White Quail Flying Crew Neck Sweater",
+    image: w71,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Westbound Geometric Jacket",
+    image: y21,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Silk Black Abstract Shields Versace Style Bomber Jacket",
+    image: y11,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default data_product;
