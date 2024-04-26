fbe81f8b2a58461d631b4edd1cd646463fb9501b
Leslie Orellana <lesore0789@gmail.com>
2024-04-25 22:50:34 -0700

    updated assets

--- frontend/src/Components/Assets/allProducts.js
+++ frontend/src/Components/Assets/allProducts.js
@@ -1,327 +1,543 @@
 import v11 from "./images/v11.jpg";
+import v12 from "./images/v12.jpg";
+import v13 from "./images/v13.jpg";
 import v21 from "./images/v21.jpg";
+import v22 from "./images/v22.jpg";
+import v23 from "./images/v23.jpg";
 import v31 from "./images/v31.jpg";
+import v32 from "./images/v32.jpg";
+import v33 from "./images/v33.jpg";
 import v41 from "./images/v41.jpg";
+import v42 from "./images/v42.jpg";
+import v43 from "./images/v43.jpg";
 import v51 from "./images/v51.jpg";
+import v52 from "./images/v52.jpg";
+import v53 from "./images/v53.jpg";
 import v61 from "./images/v61.jpg";
+import v62 from "./images/v62.jpg";
+import v63 from "./images/v63.jpg";
 import v71 from "./images/v71.jpg";
+import v72 from "./images/v72.jpg";
+import v73 from "./images/v73.jpg";
 import v81 from "./images/v81.jpg";
+import v82 from "./images/v82.jpg";
+import v83 from "./images/v83.jpg";
 import v91 from "./images/v91.jpg";
+import v92 from "./images/v92.jpg";
+import v93 from "./images/v93.jpg";
 import w11 from "./images/w11.jpg";
+import w12 from "./images/w12.jpg";
+import w13 from "./images/w13.jpg";
 import w21 from "./images/w21.jpg";
+import w22 from "./images/w22.jpg";
+import w23 from "./images/w23.jpg";
 import w31 from "./images/w31.jpg";
+import w32 from "./images/w32.jpg";
+import w33 from "./images/w33.jpg";
 import w41 from "./images/w41.jpg";
+import w42 from "./images/w42.jpg";
+import w43 from "./images/w43.jpg";
 import w51 from "./images/w51.jpg";
+import w52 from "./images/w52.jpg";
+import w53 from "./images/w53.jpg";
 import w61 from "./images/w61.jpg";
+import w62 from "./images/w62.jpg";
+import w63 from "./images/w63.jpg";
 import w71 from "./images/w71.jpg";
+import w72 from "./images/w72.jpg";
+import w73 from "./images/w73.jpg";
 import w81 from "./images/w81.jpg";
+import w82 from "./images/w82.jpg";
+import w83 from "./images/w83.jpg";
 import w91 from "./images/w91.jpg";
+import w92 from "./images/w92.jpg";
+import w93 from "./images/w93.jpg";
 import x11 from "./images/x11.jpg";
+import x12 from "./images/x12.jpg";
+import x13 from "./images/x13.jpg";
 import x21 from "./images/x21.jpg";
+import x22 from "./images/x22.jpg";
+import x23 from "./images/x23.jpg";
 import x31 from "./images/x31.jpg";
+import x32 from "./images/x32.jpg";
+import x33 from "./images/x33.jpg";
 import x41 from "./images/x41.jpg";
+import x42 from "./images/x42.jpg";
+import x43 from "./images/x43.jpg";
 import x51 from "./images/x51.jpg";
+import x52 from "./images/x52.jpg";
+import x53 from "./images/x53.jpg";
 import x61 from "./images/x61.jpg";
+import x62 from "./images/x62.jpg";
+import x63 from "./images/x63.jpg";
 import x71 from "./images/x71.jpg";
+import x72 from "./images/x72.jpg";
+import x73 from "./images/x73.jpg";
 import x81 from "./images/x81.jpg";
+import x82 from "./images/x82.jpg";
+import x83 from "./images/x83.jpg";
 import x91 from "./images/x91.jpg";
+import x92 from "./images/x92.jpg";
+import x93 from "./images/x93.jpg";
 import y11 from "./images/y11.jpg";
+import y12 from "./images/y12.jpg";
+import y13 from "./images/y13.jpg";
 import y21 from "./images/y21.jpg";
+import y22 from "./images/y22.jpg";
+import y23 from "./images/y23.jpg";
 import y31 from "./images/y31.jpg";
+import y32 from "./images/y32.jpg";
+import y33 from "./images/y33.jpg";
 import y41 from "./images/y41.jpg";
+import y42 from "./images/y42.jpg";
+import y43 from "./images/y43.jpg";
 import y51 from "./images/y51.jpg";
+import y52 from "./images/y52.jpg";
+import y53 from "./images/y53.jpg";
 import y61 from "./images/y61.jpg";
+import y62 from "./images/y62.jpg";
+import y63 from "./images/y63.jpg";
 import y71 from "./images/y71.jpg";
+import y72 from "./images/y72.jpg";
+import y73 from "./images/y73.jpg";
 import y81 from "./images/y81.jpg";
+import y82 from "./images/y82.jpg";
+import y83 from "./images/y83.jpg";
 import y91 from "./images/y91.jpg";
+import y92 from "./images/y92.jpg";
+import y93 from "./images/y93.jpg";
 // import z11 from "./images/z11.jpg";
 
 let all_product = [
   {
     id: 1,
     name: "Navy Blue Graphic Print Pattern Polo Shirt ",
+    description:
+      "Vintage 90s Navy Blue Green Red White Yellow Country Flag Graphic Print Pattern Polo Shirt Flags Inner Terry Cloth Zip Up Shirt Size XL",
     category: "men",
     image: v11,
+    image2: v12,
+    image3: v13,
     new_price: 50.0,
     old_price: 80,
   },
   {
     id: 2,
     name: "Brown Abstract Geometric Print Pattern Short Sleeve Polo Shirt",
+    description:
+      "Vintage 80s Brown Abstract Geometric Print Pattern Short Sleeve Polo Shirt Grandpa Dad Style Dorky Garterized Hem Button Polo Size Medium",
     category: "men",
     image: v21,
+    image2: v22,
+    image3: v23,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 3,
     name: "Colorful Trees Print Short Sleeve Button Down",
+    description:
+      "Vintage Y2K Colorful Trees Leaves Nature Abstract Print Short Sleeve Button Down Tropical Grunge Summer Beach Miami Vice Shirt Size Medium",
     category: "men",
     image: v31,
+    image2: v32,
+    image3: v33,
     new_price: 60.0,
     old_price: 100.0,
   },
   {
     id: 4,
     name: "Geometric Birds Print Multicolor Short Sleeve",
+    description:
+      "Vintage 90s Abstract Geometric Birds Print Pattern Multicolor Short Sleeve Saved By The Bell Fresh Prince Button Down Colorful Shirt Size M",
     category: "men",
     image: v41,
+    image2: v42,
+    image3: v43,
     new_price: 100.0,
     old_price: 150.0,
   },
   {
     id: 5,
     name: "Playboy Logo White Polo Shirt",
+    description:
+      "Vintage 90s Playboy Logo White Polo Shirt With V-shaped Striped Stripe Detail Dorky Dad Preppy Minimalist Shirt Size Medium",
     category: "men",
     image: v51,
+    image2: v52,
+    image3: v53,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 6,
     name: "NIKE Rare Denim Vintage Jacket Distressed/Acid Wash",
+    description:
+      "Vintage 90s NIKE Rare Denim Vintage Nike Challenge Court Denim Jacket Distressed Acid Wash Athletic Jacket Size 2X",
     category: "men",
     image: v61,
+    image2: v62,
+    image3: v63,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 7,
     name: "White PUMA Lightweight Windbreaker Colorful Striped Detail",
+    description:
+      "Vintage 90s Sheer White PUMA Lightweight Jacket Windbreaker Colorful Striped Detail Zip Up Blue Stretchy Cuffs & Hem Track Jacket Size Small",
     category: "men",
     image: v71,
+    image2: v72,
+    image3: v73,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 8,
     name: "Shiny Gold Pullman Players Ramada Bomber Jacket",
+    description:
+      "Vintage 90s Shiny Gold Pullman Players Ramada Express Graphic Print Bomber Jacket Laughlin Nevada Snap Button Casino Jacket Size Large",
     category: "men",
     image: v81,
+    image2: v82,
+    image3: v83,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 9,
     name: "Yellow Members Only Windbreaker Jacket",
+    description:
+      "Vintage 90s Yellow Members Only Windbreaker Jacket Full Zip Moto Shoulder Neck Strap Snap Button Bomber Lightweight Jacket Size 3X",
     category: "men",
     image: v91,
+    image2: v92,
+    image3: v93,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 10,
     name: "Abstract Geometric Pattern Print Windbreaker",
+    description:
+      "Vintage 90s Color Block Abstract Geometric Pattern Print Windbreaker Multicolor Tassel Zipper Stretchy Hem Athletic Track Jacket Size Large",
     category: "men",
     image: w11,
+    image2: w12,
+    image3: w13,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 11,
     name: "Purple Windbreaker Neon Line Detail",
+    description:
+      "Vintage 90s Quick Silver Ocean Tribe Color Block Purple Windbreaker Neon Line Detail Embroidered Multicolor Quarter Zip Track Jacket Size XL",
     category: "men",
     image: w21,
+    image2: w22,
+    image3: w23,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 12,
     name: "Eddie Bauer Teal & Navy Blue Jacket",
+    description:
+      "Vintage 90s Eddie Bauer Teal Navy Blue Purple Sweater Bomber Jacket Style Full Zip String Knot Athletic Track Outdoor Jacket Size XL",
     category: "men",
     image: w31,
+    image2: w32,
+    image3: w33,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 13,
     name: "Black W/ Pastel Pink/Teal Embroidered Mako's Shark Bomber Jacket",
+    description:
+      "Vintage 90s Black W/ Pastel Pink/Teal Embroidered Mako's Shark Bomber Jacket Snap Button Nautical Ocean Animal Promo Jacket Size Medium",
     category: "men",
     image: w41,
+    image2: w42,
+    image3: w43,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 14,
     name: "Purple With Teal And Black Striped Lining Detail Windbreaker",
+    description:
+      "Vintage 90s Purple With Teal And Black Striped Lining Detail Windbreaker Embroidered Tennis Ball & Racket Full Zip Sports Jacket Size 2X",
     category: "men",
     image: w51,
+    image2: w52,
+    image3: w53,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 15,
     name: "Burgundy/Maroon Lightweight Jacket",
+    description:
+      "Vintage 90s Sport Abouts Jacket Burgundy Red Maroon Zip Up Moto Biker Shoulder Neck Strap Snap Button Bomber Lightweight Jacket Size Large",
     category: "men",
     image: w61,
+    image2: w62,
+    image3: w63,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 16,
     name: "Flying Birds Blue Graphic Print Crew Neck Sweater",
+    description:
+      "Vintage 90s Bob White Quail Flying Birds NWF Blue Graphic Print Crew Neck Sweater National Wildlife Federation Nature Sweatshirt Size Medium",
     category: "men",
     image: w71,
+    image2: w72,
+    image3: w73,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 17,
     name: "Grandma Core Dark Pink Graphic Print Crew Neck Sweatshirt",
+    description:
+      "Vintage 90s Grandma Core Country Cottage Precious Treasures Dark Pink Graphic Print Crew Neck Sweatshirt Cute Pullover Sweater Size Size",
     category: "women",
     image: w81,
+    image2: w82,
+    image3: w83,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 18,
     name: "Black San Diego California Graphic Print Hoody Sweater",
+    description:
+      "Vintage 90s Black San Diego California Graphic Print Hoody Sweater Patchwork Style Color Block Palm Trees Pullover Hoodie Sweatshirt Size M",
     category: "women",
     image: w91,
+    image2: w92,
+    image3: w93,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 19,
     name: "Colorful Striped Pattern Ribbed Knit Pullover Sweater",
+    description:
+      "Vintage 90s Colorful Striped Pattern Ribbed Knit Pullover Sweater Cable Knit Knitted Preppy Color Block Rainbow Crew Neck Sweater SizeMedium",
     category: "women",
     image: x11,
+    image2: x12,
+    image3: x13,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 20,
     name: "Sheepskin Copper Orange Faux Fur Lined Collar Leather Jacket",
+    description:
+      "Vintage 70s Wilsons Sheepskin Copper Orange Faux Fur Lined Collar Leather Jacket Shiny WesternWomen's Bike Motorcycle Jacket Size Small",
     category: "women",
     image: x21,
+    image2: x22,
+    image3: x23,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 21,
     name: "Color Block Adidas Embroidered Logo Geometric Retro Pattern Sweater",
+    description:
+      "Vintage 90s Color Block Adidas Embroidered Logo Geometric Retro Pattern Sweater Colorful Full Zip Sporty Multicolor Sweater Jacket Size 3X",
     category: "women",
     image: x31,
+    image2: x32,
+    image3: x33,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 22,
     name: "White Lisa Members Only American Embassy Lounge Bomber Jacket",
+    description:
+      "Vintage 90s White Lisa Members Only American Embassy Lounge Bomber Jacket Back Embroidered Eagle Stars Zip Up Strap Neck Jacket Size XL",
     category: "women",
     image: x41,
+    image2: x42,
+    image3: x43,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 23,
     name: "Blue Denim Jean Jacket Snap Button Shoulder Strap Striped String Knot Collar",
+    description:
+      "Vintage 90s Blue Denim Jean Jacket Snap Button Shoulder Strap Striped String Knot Collar Multi Pockets Western Women's Jean Jacket Size M",
     category: "women",
     image: x51,
+    image2: x52,
+    image3: x53,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 24,
     name: "Reversible Purple Nautical Pattern Print Jacket",
+    description:
+      "Vintage 90s Reversible Purple Nautical Pattern Print Jacket Reverse Versace Style Snap Button Zip Up Girly Bomber Style Jacket Size XL",
     category: "women",
     image: x61,
+    image2: x62,
+    image3: x63,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 25,
     name: "Brown Geometric Abstract Pattern Jacket",
+    description:
+      "Vintage 90s Brown Geometric Abstract Pattern Detail Color Block Jacket Garterized Stretchy Waist String Knot Zip Up Windbreaker Size 2X",
     category: "women",
     image: x71,
+    image2: x72,
+    image3: x73,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 26,
     name: "Brown Velvet Silver Bejeweled Grunge Button Up Jacket",
+    description:
+      "Vintage 90s Brown Velvet Silver Bejeweled Grunge Button Up Jacket Jewels Studded Women's Work Office Blazer Jacket Size Medium",
     category: "women",
     image: x81,
+    image2: x82,
+    image3: x83,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 27,
     name: "Silk Navy Blue Plain Front Multicolor USA Flag Print Geometric Back Pattern Windbreaker",
+    description:
+      "Vintage 90s 100% Silk Navy Blue Plain Front Multicolor USA Flag Print Geometric Back Pattern Windbreaker Stars Full Zip Jacket Size Medium",
     category: "women",
     image: x91,
+    image2: x92,
+    image3: x93,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 28,
     name: "Silk Red & Gold Versace Style Tapestry Pattern Print Bomber Jacket",
+    description:
+      "Vintage 90s 100% Silk Red & Gold Versace Style Tapestry Pattern Print Bomber Jacket Full Zip Oleg Cassini Blue Back Windbreaker Size Medium",
     category: "women",
     image: y11,
+    image2: y12,
+    image3: y13,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 29,
     name: "Silk Black Abstract Shields Bomber Jacket",
+    description:
+      "Vintage 90s 100% Silk Black Abstract Shields Versace Style Tapestry Print Pattern Bomber Jacket Full Zip Funky Windbreaker Size XL",
     category: "women",
     image: y21,
+    image2: y22,
+    image3: y23,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 30,
     name: "Westbound Green Geometric Pattern Patchwork Colorful Windbreaker Jacket",
+    description:
+      "Vintage 90s Westbound Green Geometric Pattern Patchwork Colorful Windbreaker Jacket Zip Up Triangle Shapes Multicolor Track Jacket Size 2X",
     category: "women",
     image: y31,
+    image2: y32,
+    image3: y33,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 31,
     name: "Abstract Tapestry Pattern Print Burgundy Red Bomber Jacket",
+    description:
+      "Vintage 90s Abstract Versace Style Tapestry Pattern Print Burgundy Red Bomber Jacket Full Zip Up Fashion Puffy Windbreaker Jacket Size XL",
     category: "women",
     image: y41,
+    image2: y42,
+    image3: y43,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 32,
     name: "Pink Abstract Geometric Shapes Pattern Windbreaker Jacket",
+    description:
+      "Vintage 90s Pink Abstract Geometric Shapes Pattern Windbreaker Jacket Zip Up Multicolor Stretchy Waist & Cuffs Track Jacket Size Medium",
     category: "women",
     image: y51,
+    image2: y52,
+    image3: y53,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 33,
     name: "Colorful Floral Flowers Print Pattern Windbreaker Jacket",
+    description:
+      "Vintage 90s Colorful Floral Flowers Print Pattern Windbreaker Jacket Multicolor Modern V-Neck Full Zip Funky Track Jacket Size XL",
     category: "women",
     image: y61,
+    image2: y62,
+    image3: y63,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 34,
     name: "Multicolor Geometric Triangle Pattern Windbreaker",
+    description:
+      "Vintage 90s Color Block Multicolor Geometric Triangle Pattern Windbreaker Full Zip Purple Stretchy Waist Athletic Track Jacket Size 2X",
     category: "women",
     image: y71,
+    image2: y72,
+    image3: y73,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 35,
     name: "Floral Print Pattern Windbreaker Colorful Full Zip Jacket",
+    description:
+      "Vintage 90s Color Block Abstract Pastel Floral Print Pattern Windbreaker Colorful Full Zip Multicolor Preppy Womens Track Jacket Size Medium",
     category: "women",
     image: y81,
+    image2: y82,
+    image3: y83,
     new_price: 85.0,
     old_price: 120.0,
   },
   {
     id: 36,
     name: "Green Jewels & Tassel Versace Style Print Pattern Colorful Windbreaker",
+    description:
+      "Vintage 90s Green Jewels & Tassel Versace Style Print Pattern Colorful Windbreaker Fancy Multicolor Full Zip Jacket Size Petite Medium",
     category: "women",
     image: y91,
+    image2: y92,
+    image3: y93,
     new_price: 85.0,
     old_price: 120.0,
   },
